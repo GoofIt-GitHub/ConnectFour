558d324f9827f03aa9a55ec9b8247dbbbf18602c
Matthew <matthew.mcroberts22@gmail.com>
2024-05-30 13:26:49 -0400

    comments

--- src/main/java/com/matthew/plugin/connectfour/apis/ServerModule.java
+++ src/main/java/com/matthew/plugin/connectfour/apis/ServerModule.java
@@ -2,7 +2,13 @@ package com.matthew.plugin.connectfour.apis;
 
 public interface ServerModule {
 
+    /**
+     * Allocate necessary resources into memory for the module
+     */
     void setup();
 
+    /**
+     * Release any allocated resources that are still currently stored in memory
+     */
     void teardown();
 }
